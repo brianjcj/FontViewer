c37b35d2d1f008e39359721ba5ff787b4a4fd363
蒋承军 <brianjcj@gmail.com>
2014-03-06 06:55:59 +0800

    the real first commit.

--- FontViewer/ViewController.h
+++ FontViewer/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- FontViewer/ViewController.m
+++ FontViewer/ViewController.m
@@ -13,11 +13,45 @@
 @end
 
 @implementation ViewController
+{
+    NSString *_text;
+    NSMutableArray *_fontFamilies;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    _fontFamilies = [NSMutableArray new];
+    _text = @"中文字体";
+
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+
+    [self listFonts];
+}
+
+
+- (void) listFonts
+{
+    NSArray *familyNames = [UIFont familyNames];
+    for( NSString *familyName in familyNames ){
+        //NSLog(@"Family: %@", familyName);
+        printf( "Family: %s \n", [familyName UTF8String] );
+        NSArray *fontNames = [UIFont fontNamesForFamilyName:familyName];
+
+        NSMutableArray *fonts = [NSMutableArray new];
+        for( NSString *fontName in fontNames ){
+            //NSLog(@"\tFamily: %@", fontName);
+            printf( "\tFont: %s \n", [fontName UTF8String] );
+
+            [fonts addObject:[UIFont fontWithName:fontName size:20]];
+        }
+
+        [_fontFamilies addObject:fonts];
+    }
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +59,63 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return _fontFamilies.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_fontFamilies[section] count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    UIFont *font = _fontFamilies[section][0];
+    return font.familyName;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [_tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+
+    UIFont *font = _fontFamilies[indexPath.section][indexPath.row];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@-%@,%@", _text, font.fontName, font.familyName];
+    cell.textLabel.font = font;
+
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@,%@", font.fontName, font.familyName];
+
+    return cell;
+
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UIFont *font = _fontFamilies[indexPath.section][indexPath.row];
+
+    self.navigationItem.title = font.fontName;
+
+}
+
+- (IBAction)changeText:(id)sender
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Change Text" message:@"" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
+    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+
+    [alertView show];
+}
+
+- (void) alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex != -1) {
+        _text = [alertView textFieldAtIndex:0].text;
+
+        [_tableView reloadData];
+    }
+}
+
 
 @end
